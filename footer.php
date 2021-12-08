80bc907ee03735f33e61e0bb75c16c6d4619a450
Pat Monette <patrick@ontarioseo.ca>
2021-12-08 20:00:21 +0000

    footer column mobile updates

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 		<div class="footer-widgets">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-7">
+					<div class="col-lg-7">
 						<div class="row">
 							<div class="col-md-6">
 								<h4 style="margin-bottom:3rem;">ASAP Pest Control Inc.</h4>
@@ -32,7 +32,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 								<img class="p-5" src="/wp-content/themes/asappest/images/knights-logo.png" alt="Official Pest Control of the London Knights">
 							</div>
 
-							<div class="col-md-6">
+							<div class="col-md-6 mb-3">
 								<h4 style="margin-top:2rem;">Testimonials</h4>
 								<p><i>“Jamie Drennan has been providing pest control service (at our store) location. The extent of infestation was significant and Jamie has made weekly visits to ensure the rodent population was brought down to a reasonable threshold. During this time, Jamie has been extremely helpful, and courteous while providing these services, knowledgeable and friendly.”</i></p>
 								<a href="/testimonials/" class="btn btn-large btn-primary">Read More Testimonials</a>
@@ -43,7 +43,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 							</div>
 						</div>
 					</div>
-					<div class="col-md-5">
+					<div class="col-lg-5">
 						<h4>Quick Contact:</h4>
 						<?php echo do_shortcode('[gravityform id="2" title="false"]'); ?>
 					</div>
