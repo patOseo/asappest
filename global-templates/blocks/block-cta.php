7690260aefa38e0e131e8916a95c2b79e18e4475
Pat Monette <patrick@ontarioseo.ca>
2022-05-10 14:37:43 +0000

    cta click-to-call block

--- global-templates/blocks/block-cta.php
+++ global-templates/blocks/block-cta.php
@@ -1,6 +1,7 @@
 <?php
 $cta = get_field('call-to-action');
 $link = get_field('link');
+$ctc = get_field('click-to-call');
 $base_url = esc_url(home_url( '/' ));
 $str_link = str_replace($base_url, '', $link);
 $size = get_field('size');
@@ -11,5 +12,5 @@ $alignment = get_field('alignment');
 ?>
 
 <div class="call-to-action <?php echo $alignment; ?>">
-	<a class="btn btn-primary btn-<?php echo $size; ?> <?php if($color): ?>btn-<?php echo $color; endif; ?>" href="<?php echo $link; ?>"><?php echo $cta; ?></a>
+	<a class="btn btn-primary btn-<?php echo $size; ?> <?php if($color): ?>btn-<?php echo $color; endif; ?>" href="<?php if($ctc == 1): echo "tel:" . get_field('phone_number', 'option'); else: echo $link; endif; ?>"><?php echo $cta; ?></a>
 </div>
